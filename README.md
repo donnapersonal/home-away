a6f26cd877950ef3d9df658b0fb1ef60c3612912
donnapersonal <tange0530@gmail.com>
2024-11-10 20:11:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+### Home away
+
+启动：
+
+```sh
+npm run dev
+```
+
+#### Documents
+
+- Shadcn/ui 
+  - [Docs](https://ui.shadcn.com/)
+  - [Next Install](https://ui.shadcn.com/docs/installation/next)
+  - [Next.js Dark Mode](https://ui.shadcn.com/docs/dark-mode/next)
+  - [Theming Options](https://ui.shadcn.com/docs/theming)
+  - [Themes](https://ui.shadcn.com/themes)
+- [React Icons](https://react-icons.github.io/react-icons/)
+- Clerk
+  - [Clerk Docs](https://clerk.com/)
+  - [Clerk + Next.js Setup](https://clerk.com/docs/quickstarts/nextjs)
+- [Prisma Instance](https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices#solution)
+- Connect Supabase with Prisma
+  - [Useful Info](https://supabase.com/partners/integrations/prisma)
+- [Prisma Docs](https://www.prisma.io/docs/concepts/components/prisma-client/crud)
+- prisma
+  
+  ```bash
+  npx prisma db push
+  npx prisma studio
+  ```
+- [Clerk User Metadata](https://clerk.com/docs/users/metadata)
+- [React Share](https://www.npmjs.com/package/react-share)
+- [React Leaflet](https://react-leaflet.js.org/)
---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,71 @@
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import PropertyRating from "@/components/card/PropertyRating";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
+import ShareButton from "@/components/properties/ShareButton";
+import BookingCalendar from "@/components/booking/BookingCalendar";
+import { fetchPropertyDetails } from "@/utils/actions";
+import { redirect } from "next/navigation";
+import UserInfo from "@/components/properties/UserInfo";
+import { Separator } from "@/components/ui/separator";
+import Description from "@/components/properties/Description";
+import Amenities from "@/components/properties/Amenities";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+
+// Lazy Loading
+// Server Side Rendering (SSR) Control
+// By default, Next.js pre-renders every page.
+// - This means that it generates HTML for each page in advance, instead of doing it all on the client-side.
+// - However, with dynamic, you can control this behavior.
+// - You can choose to disable SSR for specific modules, which can be useful for modules that have client-side dependencies.
+const DynamicMap = dynamic(
+  () => import('@/components/properties/PropertyMap'),
+  {
+    ssr: false,
+    loading: () => <Skeleton className='h-[400px] w-full' />,
+  }
+);
+
+async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect("/");
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
+
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold ">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          <ShareButton name={property.name} propertyId={property.id} />
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+          <PropertyDetails details={details} />
+          <UserInfo profile={{ firstName, profileImage }} />
+          <Separator className='mt-4' />
+          <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          <BookingCalendar />
+        </div>
+      </section>
+    </section>
+  );
+}
+
+export default PropertyDetailsPage;
